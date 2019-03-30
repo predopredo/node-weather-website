f343c19585bff66c3cba1bad00b3d9102ef9a1c2
Pedro Santos <pedrosantosrochap@gmail.com>
2019-03-30 18:50:18 -0300

    tries to use local client time instead of server time

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,14 @@ const messageTwo = document.querySelector('#message-2');
 const iconDisplay = document.querySelector('#icon');
 const timeDisplay = document.querySelector('#time');
 
+function prettyDate2() {
+  var date = new Date();
+  return date.toLocaleTimeString(navigator.language, {
+    hour: '2-digit',
+    minute:'2-digit'
+  });
+}
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     messageOne.innerHTML = `<img id="loading" src="/img/ajax-loader.png">`;
@@ -19,10 +27,10 @@ weatherForm.addEventListener('submit', (e) => {
             if (data.error) {
                 return messageOne.textContent = data.error;
             }
-
+            const now = prettyDate2()
             messageOne.innerHTML = `Forecast for: ${data.location}`;
             messageTwo.textContent = data.forecast;
-            timeDisplay.textContent = `Requested at ${data.time}`;
+            timeDisplay.textContent = `Requested at ${now}`;
 
             iconDisplay.setAttribute('src', `/img/icons/${data.icon}.png`);
             iconDisplay.setAttribute('alt', data.icon);
